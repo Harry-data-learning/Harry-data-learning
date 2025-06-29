1c9c25b1ca8a6d12784a40ee95bb0d6baed74e96
Harry_JY <zhangweiharry818@gmail.com>
2025-06-30 00:07:32 +1100

    Update IBM 4 Sql database.py

--- IBM 4 Sql database.py
+++ IBM 4 Sql database.py
@@ -51,18 +51,18 @@ for row in rows:
 print("\nRunning example SQL queries:\n")
 
 queries = [
-    ("Unique Launch Sites", "SELECT DISTINCT LaunchSite FROM SPACEXTABLE"),
-    ("Launches from CCA sites", "SELECT * FROM SPACEXTABLE WHERE LaunchSite LIKE 'CCA%' LIMIT 5"),
+    ("Unique Launch Sites", "SELECT DISTINCT Launch_Site FROM SPACEXTABLE"),
+    ("Launches from CCA sites", "SELECT * FROM SPACEXTABLE WHERE Launch_Site LIKE 'CCA%' LIMIT 5"),
     ("Total Payload by NASA (CRS)", "SELECT SUM(PAYLOAD_MASS__KG_) FROM SPACEXTABLE WHERE Customer = 'NASA (CRS)'"),
-    ("Avg Payload for F9 v1.1", "SELECT AVG(PAYLOAD_MASS__KG_) FROM SPACEXTABLE WHERE BoosterVersion = 'F9 v1.1'"),
+    ("Avg Payload for F9 v1.1", "SELECT AVG(PAYLOAD_MASS__KG_) FROM SPACEXTABLE WHERE Booster_Version = 'F9 v1.1'"),
     ("Earliest ground pad landing", "SELECT MIN(Date) FROM SPACEXTABLE WHERE Landing_Outcome = 'Success (ground pad)'"),
     ("Boosters with 4000-6000kg Payload & success on drone ship", 
-     "SELECT BoosterVersion FROM SPACEXTABLE WHERE Landing_Outcome = 'Success (drone ship)' AND PAYLOAD_MASS__KG_ BETWEEN 4000 AND 6000"),
+     "SELECT Booster_Version FROM SPACEXTABLE WHERE Landing_Outcome = 'Success (drone ship)' AND PAYLOAD_MASS__KG_ BETWEEN 4000 AND 6000"),
     ("Landing outcome counts", "SELECT Landing_Outcome, COUNT(*) FROM SPACEXTABLE GROUP BY Landing_Outcome"),
     ("Booster with max payload", 
-     "SELECT BoosterVersion, PAYLOAD_MASS__KG_ FROM SPACEXTABLE WHERE PAYLOAD_MASS__KG_ = (SELECT MAX(PAYLOAD_MASS__KG_) FROM SPACEXTABLE)"),
+     "SELECT Booster_Version, PAYLOAD_MASS__KG_ FROM SPACEXTABLE WHERE PAYLOAD_MASS__KG_ = (SELECT MAX(PAYLOAD_MASS__KG_) FROM SPACEXTABLE)"),
     ("Monthly launches in 2015", 
-     "SELECT substr(Date,6,2) AS Month, Landing_Outcome, BoosterVersion, LaunchSite FROM SPACEXTABLE WHERE substr(Date,1,4) = '2015'"),
+     "SELECT substr(Date,6,2) AS Month, Landing_Outcome, Booster_Version, Launch_Site FROM SPACEXTABLE WHERE substr(Date,1,4) = '2015'"),
     ("Landing outcomes between dates (2010-06-04 ~ 2017-03-20)",
      "SELECT Landing_Outcome, COUNT(*) FROM SPACEXTABLE WHERE Date BETWEEN '2010-06-04' AND '2017-03-20' GROUP BY Landing_Outcome ORDER BY COUNT(*) DESC")
 ]
